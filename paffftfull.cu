55ecfc8dfa70303bbc6abddc1b22a2ba37033041
Mateusz Malenta <mateusz.malenta@gmail.com>
2015-10-19 16:31:52 +0100

    Put async fft plan ceation in the right place.

--- paffftfull.cu
+++ paffftfull.cu
@@ -211,6 +211,13 @@ int main(int argc, char* argv[])
 		cudaStreamCreate(&stream1);
 		cudaStreamCreate(&stream2);
 
+		cufftHandle multiplans1, multiplans2;
+		geterror(cufftPlanMany(&multiplans1, 1, sizes, NULL, 1, 0, NULL, 1, 0, CUFFT_C2C, batchsize), "async plan make 1");
+		geterror(cufftPlanMany(&multiplans2, 1, sizes, NULL, 1, 0, NULL, 1, 0, CUFFT_C2C, batchsize), "async plan make 2");
+
+		geterror(cufftSetStream(multiplans1, stream1), "FFT set stream 1");
+		geterror(cufftSetStream(multiplans2, stream2), "FFT set stream 2");
+
 		// must use pinned memory
 		// very crude memory managment here
 		// this will end up in loops at the end of the day
@@ -236,13 +243,6 @@ int main(int argc, char* argv[])
 
 		geterror(cudaMemcpyAsync(d_inarray1, h_inarraya1, memsize, cudaMemcpyHostToDevice, stream1), "HtD async copy 1");
 		geterror(cudaMemcpyAsync(d_inarray2, h_inarraya2, memsize, cudaMemcpyHostToDevice, stream2), "HtD async copy 2");
-
-		cufftHandle multiplans1, multiplans2;
-		geterror(cufftPlanMany(&multiplans1, 1, sizes, NULL, 1, 0, NULL, 1, 0, CUFFT_C2C, batchsize), "async plan make 1");
-		geterror(cufftPlanMany(&multiplans2, 1, sizes, NULL, 1, 0, NULL, 1, 0, CUFFT_C2C, batchsize), "async plan make 2");
-
-		geterror(cufftSetStream(multiplans1, stream1), "FFT set stream 1");
-		geterror(cufftSetStream(multiplans2, stream2), "FFT set stream 2");
 
 		geterror(cufftExecC2C(multiplans1, d_inarray1, d_inarray1, CUFFT_FORWARD), "async execution 1");
 		geterror(cufftExecC2C(multiplans2, d_inarray2, d_inarray2, CUFFT_FORWARD), "async execution 2");
