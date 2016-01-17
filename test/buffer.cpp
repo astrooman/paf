440f689baecd1306b1ae3ab21ba37aa4735bfaf5
Mateusz Malenta <mateusz.malenta@gmail.com>
2016-01-17 00:01:22 +0000

    Fixed errors for the previous commit.

--- test/buffer.cpp
+++ test/buffer.cpp
@@ -1,10 +1,11 @@
+#include <chrono>
 #include <cstdlib>
 #include <cstring>
 #include <iostream>
 #include <vector>
 
 #include <cufft.h>
-#include <vdif_head.hpp>
+#include <vdif.hpp>
 
 #include <errno.h>
 #include <netdb.h>
@@ -16,14 +17,14 @@
 #include <unistd.h>
 #include <signal.h>
 
-#define PORT 5000
-#define BUF_LEN 8908 + 64   // 8908 bytes for sample block and 64  bytes for header
+#define PORT "5000"
 #define DATA 8908
+#define BUFLEN 8908 + 64   // 8908 bytes for sample block and 64  bytes for header
 
 using std::cout;
 using std::endl;
 
-void* get_addr(struct sockaddr *sa)
+void* get_addr(struct sockaddr *sadr)
 {
     if (sadr->sa_family == AF_INET) {
         return &(((sockaddr_in*)sadr)->sin_addr);
@@ -35,10 +36,13 @@ int main(int argc, char *argv[])
 {
     int sfd, numbytes, rv;
     addrinfo hints, *servinfo, *p;
-    sockaddr_storage their_arrd;
+    sockaddr_storage their_addr;
     socklen_t addr_len;
-    char frame[BUF_LEN];
+    char frame[BUFLEN];
     char s[INET6_ADDRSTRLEN];
+
+    std::chrono::time_point<std::chrono::system_clock> recstart, recend;
+    std::chrono::duration<double> recelapsed;
 
     memset(&hints, 0, sizeof(hints));
     hints.ai_family = AF_UNSPEC;
@@ -49,7 +53,7 @@ int main(int argc, char *argv[])
 
     for (p = servinfo; p != NULL; p = p->ai_next) {
 
-        if((sfd = socket(p-ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+        if((sfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
             cout << "Socket error\n";
             continue;
         }
@@ -79,25 +83,35 @@ int main(int argc, char *argv[])
     cufftComplex *polafull = new cufftComplex[128 * 336];
     cufftComplex *polbfull = new cufftComplex[128 * 336];
 
-    while(band < 48)
-    {
-        if ((numbytes = recvfrom(sfd, frame, MAXBUFLEN - 1, 0, (struct sockaddr*)&their_addr, &addrlen)) == -1) {
-            cout << "Error on recvfrom\n";
+    recstart = std::chrono::system_clock::now();
+
+    for (int times = 0; times < 20; times++) {
+
+        while(band < 48) {
+            if ((numbytes = recvfrom(sfd, frame, BUFLEN - 1, 0, (struct sockaddr*)&their_addr, &addr_len)) == -1) {
+                cout << "Error on recvfrom\n";
+            }
+
+            if (!numbytes)  // break on 0 bytes received for now - later process until the last frame reached
+                break;
+
+            get_header(reinterpret_cast<unsigned char*>(frame), head);
+            get_data(reinterpret_cast<unsigned char*>(frame), poladata, polbdata);
+            //band = head.frame_no % 48;
+            std::copy(poladata, poladata + 896, polafull + band * 896);
+            std::copy(polbdata, polbdata + 896, polbfull + band * 896);
+            band++;
+
+            inet_ntop(their_addr.ss_family, get_addr((sockaddr*)&their_addr), s, sizeof(s));
+
         }
 
-        if (!numbytes)  // break on 0 bytes received for now - later process until the last frame reached
-            break;
-
-        get_header(frame, head);
-        get_data(frame, poladata, polbdata)
-        //band = head.frame_no % 48;
-        std::copy(poladata, poladata + 896, polafull + band * 896);
-        std::copy(polbdata, polbdata + 896, polbfull + band * 896);
-        band++;
-
-        inet_ntop(their_addr.ss_family, get_addr((sockaddr*)&their_addr), s, sizeof(s));
-
     }
+
+    recend = std::chrono::system_clock::now();
+    recelapsed = recend - recstart;
+
+    cout << "It took " << recelapsed.count() / (double)20.0 << "s to fill one buffer\n";
 
     return 0;
 }
